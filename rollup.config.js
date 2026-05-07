966bcb1eb214e506e97d4e91cb9d0cab7a44a481
keithyoder <yoder@tessi.com.br>
2026-05-07 11:06:50 -0400

    fix packaging

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,73 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 
-export default {
-  input: 'dist/esm/index.js',
-  output: [
-    {
-      file: 'dist/plugin.js',
-      format: 'iife',
-      name: 'capacitorFlybuy',
-      globals: {
-        '@capacitor/core': 'capacitorExports',
+const external = ['@capacitor/core'];
+const plugins = [nodeResolve()];
+
+export default [
+  // Core
+  {
+    input: 'dist/esm/index.js',
+    output: [
+      {
+        file: 'dist/plugin.js',
+        format: 'iife',
+        name: 'capacitorFlybuy',
+        globals: { '@capacitor/core': 'capacitorExports' },
+        sourcemap: true,
+        inlineDynamicImports: true,
       },
-      sourcemap: true,
-      inlineDynamicImports: true,
-    },
-    {
-      file: 'dist/plugin.cjs.js',
-      format: 'cjs',
-      sourcemap: true,
-      inlineDynamicImports: true,
-    },
-  ],
-  external: ['@capacitor/core'],
-  plugins: [nodeResolve()],
-};
+      {
+        file: 'dist/plugin.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+        inlineDynamicImports: true,
+      },
+    ],
+    external,
+    plugins,
+  },
+  // Pickup
+  {
+    input: 'dist/esm/pickup/index.js',
+    output: [
+      {
+        file: 'dist/pickup/plugin.js',
+        format: 'iife',
+        name: 'capacitorFlybuyPickup',
+        globals: { '@capacitor/core': 'capacitorExports' },
+        sourcemap: true,
+        inlineDynamicImports: true,
+      },
+      {
+        file: 'dist/pickup/plugin.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+        inlineDynamicImports: true,
+      },
+    ],
+    external,
+    plugins,
+  },
+  // Notify
+  {
+    input: 'dist/esm/notify/index.js',
+    output: [
+      {
+        file: 'dist/notify/plugin.js',
+        format: 'iife',
+        name: 'capacitorFlybuyNotify',
+        globals: { '@capacitor/core': 'capacitorExports' },
+        sourcemap: true,
+        inlineDynamicImports: true,
+      },
+      {
+        file: 'dist/notify/plugin.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+        inlineDynamicImports: true,
+      },
+    ],
+    external,
+    plugins,
+  },
+];
